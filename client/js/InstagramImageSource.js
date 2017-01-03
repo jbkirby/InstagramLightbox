3f9b7f4ee1ef8be1233e137e4353de1cd757fef8
John Brian Kirby <jb@afternight.org>
2017-01-02 22:01:15 -0800

    some styling

--- client/js/InstagramImageSource.js
+++ client/js/InstagramImageSource.js
@@ -25,15 +25,26 @@ export default class InstagramImageSource extends ImageSource {
 
 	getImages() {
 		// Include 'callback' query parameter to force JSONP
-		const URL = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + this.accessToken + '&count=1&callback=?';
+		const URL = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + this.accessToken + '&count=10&callback=?';
 
 		return new Promise( (resolve, reject) => {
-			$.getJSON(URL, (result, status) => {
+			$.getJSON(URL, (results, status) => {
 				if(status !== 'success') return reject(status);
 
-				return resolve({
-					result
+				// console.log('raw result from IG: ' + JSON.stringify(results, null, 4));
+
+				let images = results.data.map( data => {
+					return {
+						// pagination : data.pagination,
+						image : {
+							standard : data.images.standard_resolution,
+							thumbnail : data.images.thumbnail
+						}//,
+						// caption : data.caption.text
+					}
 				});
+
+				return resolve(images);
 			});
 		});
 	}
--- client/js/lightbox.js
+++ client/js/lightbox.js
@@ -6,6 +6,10 @@ const INSTAGRAM_CLIENT_ID = '54cf06a9c7fa4584b9008927c8721b41';
 const URL = url.parse(window.location.href);
 const INSTAGRAM_AUTH_URL = 'https://api.instagram.com/oauth/authorize/?client_id=' + INSTAGRAM_CLIENT_ID + '&redirect_uri=' + URL.href + '&response_type=token';
 
+let imageSource = null;
+let nextImageUrl = null;
+let prevImageUrl = null;
+
 if(URL.hash === null || !URL.hash.includes('#access_token=')) {
 	// If no access token specified, redirect to instagram auth page
 	window.location = INSTAGRAM_AUTH_URL;
@@ -13,18 +17,58 @@ if(URL.hash === null || !URL.hash.includes('#access_token=')) {
 	let accessToken = URL.hash.split('=')[1];
 	console.log('access token is ' + accessToken);
 
-	let imageSource = new InstagramImageSource(accessToken);
+	imageSource = new InstagramImageSource(accessToken);
 
-	imageSource.getProfileInfo()
+	updateProfileInfo()
+	.then(loadImages)
+	.then(results => {
+		console.log('results is ' + JSON.stringify(results, null, 4));
+		// if(results.pagination) {
+		// 	if(results.pagination.next_url) {
+		// 		this.nextImageUrl = results.pagination.next_url;
+		// 	}
+		// }
+
+		$('#focus_image').attr('src', results[0].image.standard.url);
+
+		$('a#next_link').click(event => {
+			event.preventDefault();
+
+			if(nextImageUrl !== null) {
+
+			};
+		});
+
+		$('a#prev_link').click(event => {
+			event.preventDefault();
+
+			if(prevImageUrl !== null) {
+
+			};
+		});
+
+		// console.log('media query returned  ' + JSON.stringify(results, null, 4));
+	});
+}
+
+function updateProfileInfo() {
+	return imageSource.getProfileInfo()
 	.then(result => {
 		console.log('image source returned ' + JSON.stringify(result, null, 4));
 
 		$('#user_name').text(result.name);
 		$('#profile_picture').attr('src', result.profilePictureUrl);
-
-		return imageSource.getImages();
-	})
-	.then(results => {
-		console.log('media query returned ' + JSON.stringify(results, null, 4));
 	});
+}
+
+function loadImages() {
+	return imageSource.getImages();
+}
+
+function showNextPhoto() {
+
+}
+
+function showPrevPhoto() {
+
 }
